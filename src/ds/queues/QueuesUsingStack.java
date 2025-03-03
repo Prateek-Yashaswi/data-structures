738809d2927abb13f541e397fa29711d51bbce30
Prateek Yashaswi <prateekyashaswi34@gmail.com>
2025-03-04 01:58:45 +0530

    Queues using stack

---
+++ src/ds/queues/QueuesUsingStack.java
@@ -0,0 +1,56 @@
+package ds.queues;
+
+import java.util.Stack;
+
+public class QueuesUsingStack {
+
+    private Stack<Integer> stack;
+
+    public QueuesUsingStack() {
+        stack = new Stack<>();
+    }
+
+    public void enqueue(int value) {
+        stack.push(value);
+    }
+
+    public int dequeue() {
+        var temp = new Stack<Integer>();
+
+        while (!stack.isEmpty()) {
+            temp.push(stack.pop());
+        }
+
+        var removed = temp.pop();
+
+        while (!temp.isEmpty()) {
+            stack.push(temp.pop());
+        }
+
+        return removed;
+    }
+
+    public int peek() {
+        var temp = new Stack<Integer>();
+
+        while (!stack.isEmpty()) {
+            temp.push(stack.pop());
+        }
+
+        var res = temp.peek();
+
+        while (!temp.isEmpty()) {
+            stack.push(temp.pop());
+        }
+
+        return res;
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+    public int size() {
+        return stack.size();
+    }
+}
